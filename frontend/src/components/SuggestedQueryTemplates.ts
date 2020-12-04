5c46370716a55dee40ca83f5021e63a1fd719651
rhowingt@redhat.com <rhowingt@redhat.com>
2020-12-04 13:09:17 -0500

    wip for review

---
+++ frontend/src/components/SuggestedQueryTemplates.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    templates: [
+        {
+            id: 'Workloads',
+            name: 'Workloads',
+            description: 'A pre-defined search to help you review your workloads',
+            resultHeader: 'search.tile.results',
+            searchText: 'kind:daemonset,deployment,job,statefulset,replicaset',
+        },
+        {
+            id: 'search.suggested.unhealthy.name',
+            name: 'Unhealthy pods',
+            description: 'Show pods with unhealthy status',
+            resultHeader: 'table.header.status.unhealthy',
+            searchText:
+                'kind:pod status:Pending,Error,Failed,Terminating,ImagePullBackOff,CrashLoopBackOff,RunContainerError,ContainerCreating',
+        },
+        {
+            id: 'search.suggested.createdLastHour.name',
+            name: 'Created last hour',
+            description: 'Search for resources created within the last hour',
+            resultHeader: 'search.tile.results',
+            searchText: 'created:hour',
+        },
+    ],
+}
--- frontend/src/routes/SearchPage/SearchPage.tsx
+++ frontend/src/routes/SearchPage/SearchPage.tsx
@@ -1,11 +1,33 @@
 import _ from 'lodash'
-import { AcmButton, AcmPage, AcmPageHeader, AcmSearchbar } from '@open-cluster-management/ui-components'
+import {
+    AcmButton,
+    AcmPage,
+    AcmPageHeader,
+    AcmSearchbar,
+    AcmExpandableWrapper,
+    AcmCountCard,
+} from '@open-cluster-management/ui-components'
 import { PageSection } from '@patternfly/react-core'
 import '@patternfly/react-core/dist/styles/base.css'
 import React, { Fragment, useState } from 'react'
 import { searchClient } from '../../search-sdk/search-client'
-import { useSearchSchemaQuery /*, useSearchCompleteQuery*/ } from '../../search-sdk/search-sdk'
-import { /* convertStringToQuery,*/ formatSearchbarSuggestions } from './search-helper'
+import {
+    useSearchSchemaQuery,
+    useSavedSearchesQuery,
+    useSearchResultCountQuery,
+    UserSearch,
+} from '../../search-sdk/search-sdk'
+import { convertStringToQuery, formatSearchbarSuggestions } from './search-helper'
+import SuggestQueryTemplates from '../../components/SuggestedQueryTemplates'
+
+function RenderSearchPageContent() {
+    return (
+        <Fragment>
+            <RenderSearchbar />
+            <RenderSavedSearches />
+        </Fragment>
+    )
+}
 
 function RenderSearchbar() {
     const [currentQuery, setCurrentQuery] = useState('')
@@ -41,11 +63,72 @@ function RenderSearchbar() {
     )
 }
 
+function SearchResultCount(input: any, queries: any, suggestedQueryTemplates: any) {
+    const { data, error, loading } = useSearchResultCountQuery({
+        variables: { input: input },
+    })
+    if (loading) {
+        return <AcmExpandableWrapper children={<AcmCountCard loading />} withCount={false} expandable={false} />
+    } else if (error || !data || !data.searchResult) {
+        return null
+    } else if (data?.searchResult) {
+        const queriesResult = data.searchResult.slice(0, queries.length).map((query, index) => {
+            return { ...query, ...queries }
+        })
+        const suggestedQueriesResult = data.searchResult.slice(queries.length).map((query, index) => {
+            return { ...query, ...suggestedQueryTemplates[index] }
+        })
+        return (
+            <Fragment>
+                {queriesResult.length > 0 && (
+                    <Fragment>
+                        <AcmExpandableWrapper
+                            children={queries.Result.reverse()}
+                            headerLabel={'Saved searches'}
+                            withCount={true}
+                            expandable={true}
+                        />
+                    </Fragment>
+                )}
+                {suggestedQueriesResult.length > 0 && (
+                    <Fragment>
+                        <AcmExpandableWrapper
+                            children={suggestedQueriesResult}
+                            headerLabel={'Suggested search templates'}
+                            withCount={false}
+                            expandable={false}
+                        />
+                    </Fragment>
+                )}
+            </Fragment>
+        )
+    }
+}
+
+function RenderSavedSearches() {
+    const { data } = useSavedSearchesQuery({
+        client: searchClient,
+    })
+    const queries = data?.items ?? ([] as UserSearch[])
+    // each query should contain ---- description, name, results = [], resultHeader
+    const suggestedQueryTemplates = SuggestQueryTemplates?.templates ?? ([] as UserSearch[])
+    // combine the suggested queries and saved queries
+    const input = [
+        ...queries!.map((query) => convertStringToQuery(query!.searchText as string)),
+        ...suggestedQueryTemplates.map((query: { searchText: string }) => convertStringToQuery(query.searchText)),
+    ]
+    return (
+        <Fragment>
+            {/* {<SearchResultCount input={input} queries={queries} suggestedQueryTemplates={suggestedQueryTemplates} />} */}
+        </Fragment>
+    )
+}
+
 export default function SearchPage() {
     return (
         <AcmPage>
             <AcmPageHeader title="Search" />
-            <RenderSearchbar />
+            <RenderSearchPageContent />
         </AcmPage>
     )
 }
