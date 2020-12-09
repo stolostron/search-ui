97ea6bd851aa40a15914faf92d9efa1acd25caa3
Jorge Padilla <jpadilla@redhat.com>
2020-12-09 13:46:02 -0500

    Update search-client.ts

--- frontend/src/search-sdk/search-client.ts
+++ frontend/src/search-sdk/search-client.ts
@@ -2,8 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const searchClient = new ApolloClient({
     connectToDevTools: process.env.NODE_ENV === 'development',
-    // uri: `${process.env.REACT_APP_SEARCH_API_URL}/graphql`,
-    // uri: `${process.env.REACT_APP_SEARCH_API_URL || 'searchapi'}/graphql`,
     uri: 'searchapi/graphql',
     cache: new InMemoryCache(),
     credentials: 'same-origin',
