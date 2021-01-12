d0c0fc17b857959a5111508b87687fd472812520
Zack Layne <zlayne@redhat.com>
2021-01-12 17:55:49 -0500

    add more tests

--- frontend/src/routes/DetailsPage/LogsPage.test.tsx
+++ frontend/src/routes/DetailsPage/LogsPage.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { act, render, screen } from '@testing-library/react'
+import { act, render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { MockedProvider } from '@apollo/client/testing'
+import { GraphQLError } from 'graphql'
 import LogsPage from './LogsPage'
 import { GetLogsDocument } from '../../console-sdk/console-sdk'
 
@@ -12,7 +14,7 @@ async function wait(ms = 0) {
     })
 }
 
-it('should render logs page loading state', async () => {
+it('should render logs page with data and successfully switch containers', async () => {
     const mocks = [
         {
             request: {
@@ -28,7 +30,73 @@ it('should render logs page loading state', async () => {
                 data: {
                     logs: 'testLogs',
                 },
-                errors: [],
+            },
+        },
+        {
+            request: {
+                query: GetLogsDocument,
+                variables: {
+                    containerName: 'testContainer1',
+                    podName: 'testName',
+                    podNamespace: 'testNamespace',
+                    clusterName: 'testCluster',
+                },
+            },
+            result: {
+                data: {
+                    logs: 'testLogs1',
+                },
+            },
+        },
+    ]
+
+    render(
+        <MockedProvider mocks={mocks}>
+            <LogsPage
+                containers={['testContainer', 'testContainer1']}
+                cluster={'testCluster'}
+                namespace={'testNamespace'}
+                name={'testName'}
+            />
+        </MockedProvider>
+    )
+    // Test the loading state while apollo query finishes
+    expect(screen.getByText('Loading')).toBeInTheDocument()
+    // This wait pauses till apollo query is returning data
+    await wait(1)
+    // Test that the component has rendered correctly with data
+    expect(screen.getByText('testLogs')).toBeInTheDocument()
+
+    // Find and click container dropdown
+    const switchContainersButtons = screen.getAllByRole('button')
+    expect(switchContainersButtons).toBeTruthy()
+    userEvent.click(switchContainersButtons[1])
+
+    // Find an click the other container item
+    const containerItemButtons = screen.getAllByRole('option')
+    expect(containerItemButtons).toBeTruthy()
+    userEvent.click(containerItemButtons[1])
+
+    // This wait pauses till apollo query is returning data
+    await wait(1)
+    // Test that the component has rendered correctly with data
+    expect(screen.getByText('testLogs1')).toBeInTheDocument()
+})
+
+it('should render logs page in error state', async () => {
+    const mocks = [
+        {
+            request: {
+                query: GetLogsDocument,
+                variables: {
+                    containerName: 'testContainer',
+                    podName: 'testName',
+                    podNamespace: 'testNamespace',
+                    clusterName: 'testCluster',
+                },
+            },
+            result: {
+                errors: [new GraphQLError('Error getting the logs')],
             },
         },
     ]
@@ -47,6 +115,6 @@ it('should render logs page loading state', async () => {
     expect(screen.getByText('Loading')).toBeInTheDocument()
     // This wait pauses till apollo query is returning data
     await wait(1)
-    // Test that the component has rendered correctly with data
-    expect(screen.getByText('testLogs')).toBeInTheDocument()
+    // Test that the component has rendered correctly with an error
+    await waitFor(() => expect(screen.queryByText('Error getting the logs')).toBeTruthy())
 })
--- frontend/src/routes/DetailsPage/LogsPage.tsx
+++ frontend/src/routes/DetailsPage/LogsPage.tsx
@@ -1,20 +1,21 @@
 import { useState } from 'react'
 import { PageSection } from '@patternfly/react-core'
+import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
 import { AcmAlert, AcmLogWindow, AcmLoadingPage } from '@open-cluster-management/ui-components'
 import { useGetLogsQuery } from '../../console-sdk/console-sdk'
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+import { consoleClient } from '../../console-sdk/console-client'
 
 export default function LogsPage(props: {
     containers: string[]
     cluster: string
     namespace: string
     name: string
-    client?: ApolloClient<NormalizedCacheObject>
+    client?: ApolloClient<NormalizedCacheObject> // Only passed during tests via MockedProvider
 }) {
     const { containers, cluster, namespace, name } = props
     const [container, setContainer] = useState<string>(containers[0] || '')
     const { data, loading, error } = useGetLogsQuery({
-        client: props.client,
+        client: process.env.NODE_ENV === 'test' ? props.client : consoleClient,
         skip: containers.length === 0 || !container,
         variables: {
             containerName: container,
