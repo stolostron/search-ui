c796fe9447136db9ed33ae9b20c0488d95af91e5
Zack Layne <zlayne@redhat.com>
2021-01-13 11:09:34 -0500

    Add logs page unit tests (#56)
    * [WIP] add logs page unit tests
    
    * Use <ApolloProvider>
    
    * Update LogsPage.tsx
    
    * Update LogsPage.tsx
    
    * add more tests
    
    * remove commented code
    
    * remove client prop for test
    
    * review edits
    
    Co-authored-by: Jorge Padilla <jpadilla@redhat.com>

---
+++ frontend/src/routes/DetailsPage/LogsPage.test.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { act, render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { MockedProvider } from '@apollo/client/testing'
+import { GraphQLError } from 'graphql'
+import { wait } from '../../lib/test-helper'
+import LogsPage from './LogsPage'
+import { GetLogsDocument } from '../../console-sdk/console-sdk'
+
+it('should render logs page with data and successfully switch containers', async () => {
+    const mocks = [
+        {
+            request: {
+                query: GetLogsDocument,
+                variables: {
+                    containerName: 'testContainer',
+                    podName: 'testName',
+                    podNamespace: 'testNamespace',
+                    clusterName: 'testCluster',
+                },
+            },
+            result: {
+                data: {
+                    logs: 'testLogs',
+                },
+            },
+        },
+        {
+            request: {
+                query: GetLogsDocument,
+                variables: {
+                    containerName: 'testContainer1',
+                    podName: 'testName',
+                    podNamespace: 'testNamespace',
+                    clusterName: 'testCluster',
+                },
+            },
+            result: {
+                data: {
+                    logs: 'testLogs1',
+                },
+            },
+        },
+    ]
+
+    render(
+        <MockedProvider mocks={mocks}>
+            <LogsPage
+                containers={['testContainer', 'testContainer1']}
+                cluster={'testCluster'}
+                namespace={'testNamespace'}
+                name={'testName'}
+            />
+        </MockedProvider>
+    )
+    // Test the loading state while apollo query finishes
+    expect(screen.getByText('Loading')).toBeInTheDocument()
+    // This wait pauses till apollo query is returning data
+    await wait()
+    // Test that the component has rendered correctly with data
+    expect(screen.getByText('testLogs')).toBeInTheDocument()
+
+    // Find and click container dropdown
+    const switchContainersButtons = screen.getAllByRole('button')
+    expect(switchContainersButtons).toBeTruthy()
+    userEvent.click(switchContainersButtons[1])
+
+    // Find an click the other container item
+    const containerItemButtons = screen.getAllByRole('option')
+    expect(containerItemButtons).toBeTruthy()
+    userEvent.click(containerItemButtons[1])
+
+    // This wait pauses till apollo query is returning data
+    await wait()
+    // Test that the component has rendered correctly with data
+    expect(screen.getByText('testLogs1')).toBeInTheDocument()
+})
+
+it('should render logs page in error state', async () => {
+    const mocks = [
+        {
+            request: {
+                query: GetLogsDocument,
+                variables: {
+                    containerName: 'testContainer',
+                    podName: 'testName',
+                    podNamespace: 'testNamespace',
+                    clusterName: 'testCluster',
+                },
+            },
+            result: {
+                errors: [new GraphQLError('Error getting the logs')],
+            },
+        },
+    ]
+
+    render(
+        <MockedProvider mocks={mocks}>
+            <LogsPage
+                containers={['testContainer']}
+                cluster={'testCluster'}
+                namespace={'testNamespace'}
+                name={'testName'}
+            />
+        </MockedProvider>
+    )
+    // Test the loading state while apollo query finishes
+    expect(screen.getByText('Loading')).toBeInTheDocument()
+    // This wait pauses till apollo query is returning data
+    await wait()
+    // Test that the component has rendered correctly with an error
+    await waitFor(() => expect(screen.queryByText('Error getting the logs')).toBeTruthy())
+})
