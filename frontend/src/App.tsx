7f275eb83e3dad63bdc6002a3ecd920bf54c9e33
Zack Layne <zlayne@redhat.com>
2021-01-19 16:40:27 -0500

    Rework the details page URL & fix console-api queries using selflink

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,29 @@ const SearchPage = lazy(() => import('./routes/SearchPage/SearchPage'))
 const DetailsPage = lazy(() => import('./routes/DetailsPage/DetailsPage'))
 const OverviewPage = lazy(() => import('./routes/Overview/OverviewPage'))
 
+// Function to handle deprecated selfLink in old multicloud/details url
+function getUrlParams() {
+    const baseSegments = window.location.pathname.split('/').slice(3)
+    if (baseSegments[baseSegments.length - 1] === 'logs') {
+        baseSegments.pop()
+    }
+    const cluster = baseSegments[0]
+    const name = baseSegments[baseSegments.length - 1]
+    const kind = baseSegments[baseSegments.length - 2]
+    const isNamespaced = baseSegments[baseSegments.length - 4] === 'namespaces'
+    const namespace = isNamespaced ? baseSegments[baseSegments.length - 3] : ''
+    const apiversion = isNamespaced
+        ? baseSegments.slice(1, baseSegments.indexOf('namespaces')).join('/')
+        : baseSegments.slice(1, baseSegments.indexOf(kind)).join('/')
+    let resourceUrlParams = '?'
+    resourceUrlParams = `${resourceUrlParams}${cluster !== '' ? `cluster=${cluster}` : ''}`
+    resourceUrlParams = `${resourceUrlParams}${kind !== '' ? `&kind=${kind}` : ''}`
+    resourceUrlParams = `${resourceUrlParams}${apiversion !== '' ? `&apiversion=${apiversion}` : ''}`
+    resourceUrlParams = `${resourceUrlParams}${namespace !== '' ? `&namespace=${namespace}` : ''}`
+    resourceUrlParams = `${resourceUrlParams}${name !== '' ? `&name=${name}` : ''}`
+    return resourceUrlParams
+}
+
 function App() {
     return (
         <Router>
@@ -23,7 +46,10 @@ function App() {
                 <Redirect from={'/multicloud/search'} to={{ pathname: '/search', search: window.location.search }} />
                 <Redirect
                     from={'/multicloud/details'}
-                    to={{ pathname: window.location.pathname.replace('/multicloud/details', '/resources') }}
+                    to={{
+                        pathname: '/resources',
+                        search: getUrlParams(),
+                    }}
                 />
 
                 {/* Redirect to base search page on all other paths */}
