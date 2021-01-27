8f439ade08f49e5874cbfe1fc8f5452bbf915957
Jorge Padilla <jpadilla@redhat.com>
2021-01-26 23:17:34 -0500

    Update search-client.ts

--- frontend/src/search-sdk/search-client.ts
+++ frontend/src/search-sdk/search-client.ts
@@ -1,10 +1,19 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+const getXsrfToken = () => {
+    const token = (document!.head!.querySelector('meta[property=csrf-token]')! as HTMLMetaElement).content || ''
+    // const token = document.getElementById('app-access')?.value || '' // ? document.getElementById('app-access').value : ''
+    return token.toString()
+}
+
 export const searchClient = new ApolloClient({
     connectToDevTools: process.env.NODE_ENV === 'development',
     uri: 'searchapi/graphql',
     cache: new InMemoryCache(),
     credentials: 'same-origin',
+    headers: {
+        'csrf-token': getXsrfToken(),
+    },
     defaultOptions: {
         watchQuery: {
             fetchPolicy: 'network-only',
