b82bdf477386c5536fd223b971dfb8a57b1d8d5f
Zack Layne <zlayne@redhat.com>
2021-01-27 14:50:30 -0500

    fix prettier

--- backend/src/lib/logger.test.ts
+++ backend/src/lib/logger.test.ts
@@ -1,8 +1,7 @@
 import { logLevel } from './logger'
 
 describe('logger test', () => {
-
     it('should respond with correct log level', async () => {
-        expect(logLevel).toBe('silent');
-    });
-});
+        expect(logLevel).toBe('silent')
+    })
+})
