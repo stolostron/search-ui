17cef7d42b2edc60dd97507994224cbcb7dd03ae
Jorge Padilla <jpadilla@redhat.com>
2021-06-22 13:23:43 -0400

    Update unit tests

--- frontend/src/routes/SearchPage/components/HeaderWithNotification.test.tsx
+++ frontend/src/routes/SearchPage/components/HeaderWithNotification.test.tsx
@@ -3,39 +3,26 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import HeaderWithNotification from './HeaderWithNotification'
-import { MockedProvider } from '@apollo/client/testing'
 
-///case where we have a message about disabled search (current message)
+// case where we have a message about disabled search (current message)
 test('renders clusters disabled message', () => {
     const disableSearch = [
         { id: 'S20', kind: 'info', description: 'Search is disabled on some of your managed clusters.' },
     ]
-    const { getByText } = render(
-        <MockedProvider mocks={[]}>
-            <HeaderWithNotification queryMessages={disableSearch} />
-        </MockedProvider>
-    )
-    expect(getByText).toMatchSnapshot()
+    const { baseElement } = render(<HeaderWithNotification queryMessages={disableSearch} />)
+    expect(baseElement).toMatchSnapshot()
 })
 
-//case where we have no message
+// case where we have no message
 test('renders empty message', () => {
     const emptyMessage = [{}]
-    const { getByText } = render(
-        <MockedProvider mocks={[]}>
-            <HeaderWithNotification queryMessages={emptyMessage} />
-        </MockedProvider>
-    )
-    expect(getByText).toMatchSnapshot()
+    const { baseElement } = render(<HeaderWithNotification queryMessages={emptyMessage} />)
+    expect(baseElement).toMatchSnapshot()
 })
 
 // case where we have a different message
 test('renders unknown message', () => {
     const newMessage = [{ id: 'S90', kind: 'warning', message: 'This is a new message' }]
-    const { getByText } = render(
-        <MockedProvider mocks={[]}>
-            <HeaderWithNotification queryMessages={newMessage} />
-        </MockedProvider>
-    )
-    expect(getByText).toMatchSnapshot()
+    const { baseElement } = render(<HeaderWithNotification queryMessages={newMessage} />)
+    expect(baseElement).toMatchSnapshot()
 })
