ae5ee06ef05538baaed9dcd194c5fa2f507b1196
Jorge Padilla <jpadilla@redhat.com>
2021-06-22 14:10:13 -0400

    Fix unique key and add msg to translations

--- frontend/src/routes/SearchPage/components/HeaderWithNotification.tsx
+++ frontend/src/routes/SearchPage/components/HeaderWithNotification.tsx
@@ -6,43 +6,41 @@ import { useTranslation } from 'react-i18next'
 import { AcmPageHeader, AcmInlineStatus, StatusType } from '@open-cluster-management/ui-components'
 import { Card, CardBody } from '@patternfly/react-core'
 
-// 1. Needed to export as default function.
 export default function HeaderWithNotification(props: { queryMessages: any[] }) {
     const { t } = useTranslation(['search'])
     const messages: any[] = props.queryMessages
 
     return (
-        <div>
-            <div style={{ outline: 'none', display: 'flex', justifyContent: 'flex-end' }}>
-                <div style={{ flex: 1 }}>
-                    <AcmPageHeader title={t('search')} />
-                </div>
-
-                {messages?.map((msg) => {
-                    const displayShortText = t(`messages.${msg?.id}.short`)
-                    const displayLongText = t(`messages.${msg?.id}.long`)
-
-                    return (
-                        <Card style={{ border: 'none', boxShadow: 'none' }}>
-                            <CardBody>
-                                <AcmInlineStatus
-                                    type={StatusType.warning}
-                                    status={displayShortText}
-                                    popover={{
-                                        headerContent: <div>{displayShortText}</div>,
-                                        bodyContent: <div>{displayLongText}</div>,
-                                        footerContent: (
-                                            <a href='/search?filters={"textsearch":"kind%3Acluster%20addon%3Asearch-collector%3Dfalse%20name%3A!local-cluster"}'>
-                                                View disabled clusters
-                                            </a>
-                                        ),
-                                    }}
-                                />
-                            </CardBody>
-                        </Card>
-                    )
-                })}
+        <div style={{ outline: 'none', display: 'flex', justifyContent: 'flex-end' }}>
+            <div style={{ flex: 1 }}>
+                <AcmPageHeader title={t('search')} />
             </div>
+
+            {messages.map((msg) => {
+                const displayShortText = t(`messages.${msg.id}.short`) || msg?.description
+                const displayLongText = t(`messages.${msg.id}.long`) || msg?.description
+                const footerText = t(`messages.${msg.id}.additional.info`)
+
+                return (
+                    <Card key={msg.id + Math.random()} style={{ border: 'none', boxShadow: 'none' }}>
+                        <CardBody>
+                            <AcmInlineStatus
+                                type={StatusType.warning}
+                                status={displayShortText}
+                                popover={{
+                                    headerContent: displayShortText,
+                                    bodyContent: displayLongText,
+                                    footerContent: (
+                                        <a href='/search?filters={"textsearch":"kind%3Acluster%20addon%3Asearch-collector%3Dfalse%20name%3A!local-cluster"}'>
+                                            {footerText}
+                                        </a>
+                                    ),
+                                }}
+                            />
+                        </CardBody>
+                    </Card>
+                )
+            })}
         </div>
     )
 }
