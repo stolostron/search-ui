6806621ec77197815907825456e22e9f49ba75c5
Zack Layne <zlayne@redhat.com>
2021-06-23 17:22:42 -0400

    Dont allow deletion of PolicyReport resources from search page
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- frontend/src/routes/SearchPage/components/SearchResults.tsx
+++ frontend/src/routes/SearchPage/components/SearchResults.tsx
@@ -257,7 +257,7 @@ function RenderSearchTables(
                                 keyFn={(item: any) => item._uid.toString()}
                                 tableActions={[]}
                                 rowActions={
-                                    kind !== 'cluster' && kind !== 'release'
+                                    kind !== 'cluster' && kind !== 'release' && kind !== 'policyreport'
                                         ? [
                                               {
                                                   id: 'delete',
